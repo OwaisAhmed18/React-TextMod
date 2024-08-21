3578dda4dfc8056d02581a37fd10e5027a73005c
OwaisAhmed18 <owaisahmed8736@gmail.com>
2024-08-21 15:18:00 +0530

    fixed an issue

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -90,7 +90,7 @@ return (
 
 <div className="container my-3" >
 <h1>Your Text Summary</h1>
-<p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+<p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
 <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
 <h2>Preview</h2>
 <p>{text}</p>
